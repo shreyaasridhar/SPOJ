29cd3061ab83eb1ed59fc0b21319656f0ef05d05
shreyaasridhar <shreyanju@gmail.com>
2016-02-24 10:41:46 -0800

    added

---
+++ NSTEPS - Number Steps(incorrect).py
@@ -0,0 +1,13 @@
+x=input()
+for ns in range(x):
+    i,j=raw_input().split()
+    i=int(i)
+    j=int(j)
+    flag=0
+    if abs(j-i)==2 or i==j:
+        if i%2!=0 and j%2!=0:
+            print i+j-1
+        elif i%2==0 and j%2==0:
+            print  i+j
+    else:
+        print "No number"     
