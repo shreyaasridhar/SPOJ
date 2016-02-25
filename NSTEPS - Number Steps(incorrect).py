307f12ffa033ff1b5b61f7763be96f1285a3d9bd
shreyaasridhar <shreyanju@gmail.com>
2016-02-25 23:00:11 +0530

    modified

--- NSTEPS - Number Steps(incorrect).py
+++ NSTEPS - Number Steps(incorrect).py
@@ -4,10 +4,10 @@ for ns in range(x):
     i=int(i)
     j=int(j)
     flag=0
-    if abs(j-i)==2 or i==j:
+    if abs(j-i)!=2 or i!=j:
+        print "No number"
+    else:
         if i%2!=0 and j%2!=0:
             print i+j-1
         elif i%2==0 and j%2==0:
-            print  i+j
-    else:
-        print "No number"     
+            print  i+j     
